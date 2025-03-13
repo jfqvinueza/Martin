c103719bc68606f80049a500d1aabb0b2d68b676
jfqvinueza <jfq.vinueza@yavirac.edu.ec>
2025-03-13 11:14:25 -0500

    estilos integrados en cada seccion

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 async function obtenerHTML(url) {
     const respuesta = await fetch(url, {
         headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            "interest-cohort": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
         }
     });
     const html = await respuesta.text();
